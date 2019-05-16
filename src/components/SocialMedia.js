ffc5c77defcb82899cb0fb03b277446e8eae4062
MFAC <ninafroes@gmail.com>
2019-05-16 20:52:34 +0200

    feat: Add social media component

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Icon = styled.a`
+  color: silver;
+  padding: 20px;
+  font-size: 30px;
+`
+
+const SocialMedia = ({ icon, address }) => (
+  <Icon
+    href={address}
+    className="social-media"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {icon}
+  </Icon>
+);
+
+export default SocialMedia;
