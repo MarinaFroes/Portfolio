9387c8be1f0b852eeb3d5e49a9da4e5f7248b263
MFAC <ninafroes@gmail.com>
2019-05-16 22:43:25 +0200

    feat: Add timeline functionality

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import Section from './Section';
+import { Timeline, Event } from 'react-timeline-scribble';
 
-const Timeline = () => (
+
+const TimelineContainer = () => (
   <Section heading="Timeline">
-
+    <Timeline style={{ color: "red", padding: "10px 50px" }}>
+      <Event interval={"Feb 2019 – present"} title={"React Course"} subtitle={"ReDI School of Digital Integration gGmbH"}>
+        Location: Berlin - Germany
+        Curriculum: ES6 Review, Components and Props, State, Lifecycle & Handling Events, Lists & Keys, Forms.
+        Final Project (in progress): re-implementation and improvement data widgets on IDMC website
+      </Event>
+      <Event interval={"Feb 2019 – present"} title={"Front-End Web Developer Nanodegree"} subtitle={"Udacity (online)"}>
+        Curriculum: JavaScript in depth - DOM manipulation, WEB accessibility, Object-Oriented JS, Tools and Testing, Front- End Applications, etc.
+        Projects finished: Portfolio webpage, Matching Game, Classic Arcade Game Clone, Feed Reader Testing.
+        Final Project (in progress): Restaurant reviews app.
+      </Event>
+      <Event interval={"Aug 2018 – Jan 2019"} title={"Web Development Path"} subtitle={"Codecademy (online)"}>
+        Curriculum: HTML, CSS, JavaScript, Intro to React, git, etc.
+        Project: Ravenous app (Yelp clone using React)
+      </Event>
+      <Event interval={"2006 - 2011"} title={"Architecture and Urbanism"} subtitle={"Maranhao State University"}>
+        Location: São Luís - Brazil
+      </Event>
+    </Timeline>
   </Section>
 );
 
-export default Timeline;
+export default TimelineContainer;
