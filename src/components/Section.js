76422924dc82cc08663b4a975795701e4abab489
MFAC <ninafroes@gmail.com>
2019-05-16 22:44:02 +0200

    feat: Add vertical section titles

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,18 +6,19 @@ const SectionContainer = styled.section`
   margin: 0;
   width: 90vw;
   padding: 50px;
-  border: 5px solid red;
 `
 const Content = styled.div`
   display: flex;
-  border: 5px solid green;
+  text-align: left;
+  width: 100%;
 `;
 
 const Heading = styled.h1`
   text-align: left;
   margin: 0;
-  transform: rotate(-90deg);
-`;
+  writing-mode: vertical-rl;
+  text-orientation: sideways;
+`
 
 const Section = ({ heading, children }) => (
   <SectionContainer>
