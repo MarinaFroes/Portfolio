b00f83547ff1d713ce2db7ea0de00e619fd8f1ef
MFAC <ninafroes@gmail.com>
2019-05-16 18:52:18 +0200

    feat: Add section to Skill component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import { ResponsiveBar } from "@nivo/bar";
+import Section from './Section';
+import styled from 'styled-components';
 
 const BarContainer = styled.div`
-  font-size: 50px;
   height: 400px;
-  width: 80%;
-  border: 2px solid #000;
+  width: 500px;
 `;
 
 const skills = [
@@ -29,49 +28,51 @@ const skills = [
 ]
 
 const SkillsContainer = () => (
-  <BarContainer>
-    <ResponsiveBar
-      data={skills}
-      keys={['level']}
-      indexBy="skill"
-      margin={{ top: 50, right: 80, bottom: 50, left: 100 }}
-      padding={0.3}
-      maxValue={100}
-      groupMode="grouped"
-      layout="horizontal"
-      colors={{ scheme: 'category10' }}
-      borderWidth={2}
-      borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-      borderRadius={9}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        tickSize: 10,
-        tickPadding: 2,
-        tickRotation: 0,
-        legend: '',
-        legendPosition: 'middle',
-        legendOffset: 1
-      }}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: '',
-        legendPosition: 'middle',
-        legendOffset: -20
-      }}
-      enableLabel={false}
-      labelSkipWidth={20}
-      labelSkipHeight={30}
-      labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-      legends={[]}
-      isInteractive={false}
-      animate={false}
-      motionStiffness={90}
-      motionDamping={15}
-      />
-  </BarContainer>
+  <Section heading="Skills">
+    <BarContainer>
+      <ResponsiveBar
+        data={skills}
+        keys={['level']}
+        indexBy="skill"
+        margin={{ top: 50, right: 80, bottom: 50, left: 100 }}
+        padding={0.3}
+        maxValue={100}
+        groupMode="grouped"
+        layout="horizontal"
+        colors={{ scheme: 'category10' }}
+        borderWidth={2}
+        borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+        borderRadius={9}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 10,
+          tickPadding: 2,
+          tickRotation: 0,
+          legend: '',
+          legendPosition: 'middle',
+          legendOffset: 1
+        }}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: '',
+          legendPosition: 'middle',
+          legendOffset: -20
+        }}
+        enableLabel={false}
+        labelSkipWidth={20}
+        labelSkipHeight={30}
+        labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+        legends={[]}
+        isInteractive={false}
+        animate={false}
+        motionStiffness={90}
+        motionDamping={15}
+        />
+    </BarContainer>
+  </Section>
 );
 
 export default SkillsContainer;
