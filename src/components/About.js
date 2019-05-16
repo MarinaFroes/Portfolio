797e0b53042a8568f32054d15c0c6b6fdd68d6a5
MFAC <ninafroes@gmail.com>
2019-05-16 18:53:45 +0200

    feat: Add section to About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import picture from './MarinaCosta.png';
-
-const AboutContainer = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: start;
-  padding: 50px;
-`
+import Section from './Section';
 
 const Picture = styled.div`
   background-image: url(${picture});
@@ -23,24 +17,19 @@ const TextContainer = styled.div`
   padding: 30px;
 `;
 
-const SectionTitle = styled.h3`
-  font-size: 30px;
-`;
-
 const TextContent = styled.p`
   font-size: 20px;
 `;
 
 const About = () => (
-  <AboutContainer>
+  <Section heading="About Me">
     <Picture />
     <TextContainer>
-      <SectionTitle>About me</SectionTitle>
       <TextContent>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </TextContent>
     </TextContainer>
-  </AboutContainer>
+  </Section>
 );
 
 export default About;
