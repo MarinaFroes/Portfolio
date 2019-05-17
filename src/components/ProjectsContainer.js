e8bf3a4d0a7fa3e26ca726bc4209646056afe811
MFAC <ninafroes@gmail.com>
2019-05-17 11:00:35 +0200

    feat: Change project images path

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -2,33 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 import Section from './Section';
 import Project from './Project';
-import MatchingGame from './MatchingGame.png';
-import PortfolioPlainJS from './PortfolioPlainJS.png';
-import FroggerClone from './FroggerClone.png';
-import WeatherChecker from './WeatherChecker.png';
 
 const PROJECTS_INFO = [
   {
     title: "Frogger Clone",
-    src: { FroggerClone },
+    src: "/static/media/FroggerClone.da87e315.png",
     alt: "Screenshot of the Frogger Clone Project",
     link: "https://marinafroes.github.io/FEND-project3-Arcade-Game-Clone/"
   },
   {
     title: "Matching Game",
-    src: { MatchingGame },
+    src: "/static/media/MatchingGame.dac07dd2.png",
     alt: "Screenshot of the Matching Game Project",
     link: "https://marinafroes.github.io/FEND-project2-Matching-Game/"
   },
   {
-    title: "Vanilla JS Portfolio",
-    src: { PortfolioPlainJS },
+    title: "Portfolio Project",
+    src: "/static/media/PortfolioPlainJS.6dbf2d3d.png",
     alt: "Screenshot of the Vanilla JS Portfolio",
     link: "https://marinafroes.github.io/FEND-project1-Portfolio/"
   },
   {
     title: "Weather Checker",
-    src: { WeatherChecker },
+    src: "/static/media/WeatherChecker.6108e02d.png",
     alt: "Screenshot of the Weather Checker Project",
     link: "https://marinafroes.github.io/Check-Weather/"
   }
