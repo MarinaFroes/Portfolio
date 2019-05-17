28b2bdd5476e4e1cb84a48e81a95a8644014c820
MFAC <ninafroes@gmail.com>
2019-05-17 21:17:04 +0200

    feat: Add icon to send email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Section from './Section';
 import SocialMedia from './SocialMedia';
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaAt } from "react-icons/fa";
 import styled from "styled-components";
 
 const Contacts = styled.footer`
@@ -20,10 +20,10 @@ const Footer = ({ email }) => (
   <Section heading="Contact">
     <Contacts>
       <p>
-        <strong>E-mail: </strong>
-        <a href={`mailto:${email}`}>{email}</a>
+        <strong>Do you want to say hello?</strong>
       </p>
       <SocialMediaContainer>
+        <SocialMedia icon={<FaAt />} address={`mailto:${email}`} />
         <SocialMedia icon={<FaGithub />} address="https://github.com/MarinaFroes" />
         <SocialMedia icon={<FaLinkedin />} address="https://www.linkedin.com/in/marina-froes-a-costa/" />
       </SocialMediaContainer>
