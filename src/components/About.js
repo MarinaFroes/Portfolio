7f7d1a55957b23f82b8cbe0c0114ddbe381ef6a7
MFAC <ninafroes@gmail.com>
2019-05-17 10:51:17 +0200

    feat: Add constant to store About Info

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import Section from './Section';
 import Info from './Info';
-import arch from './images/arch.svg';
-import berlin from './images/berlin.svg';
-import brazil from './images/brazil.svg';
-import art from './images/art.svg';
-import languages from './images/languages.svg';
+// import arch from './images/arch.svg';
+// import berlin from './images/berlin.svg';
+// import brazil from './images/brazil.svg';
+// import art from './images/art.svg';
+// import languages from './images/languages.svg';
 
 const AboutSection = styled.div`
   display: flex;
@@ -16,14 +16,52 @@ const AboutSection = styled.div`
   width: 100%;
 `;
 
+const ABOUT_INFOS = [
+  {
+    title: "Living",
+    src: "/static/media/berlin.58b3e57b.svg",
+    alt: "",
+    subtitle: "Berlin - DE"
+  },
+  {
+    title: "From",
+    src: "/static/media/brazil.d927fc3d.svg",
+    alt: "",
+    subtitle: "Salvador - BR"
+  },
+  {
+    title: "Background",
+    src: "/static/media/arch.9dd105b2.svg",
+    alt: "",
+    subtitle: "Architecture"
+  },
+  {
+    title: "Hobbies",
+    src: "/static/media/art.aabdc549.svg",
+    alt: "",
+    subtitle: "Arts & Crafts"
+  },
+  {
+    title: "Languages",
+    src: "/static/media/languages.a49b7d60.svg",
+    alt: "",
+    subtitle: "PT(C2) - EN(B2)"
+  },
+
+];
+
 const About = () => (
   <Section heading="About Me">
     <AboutSection>
-      <Info title="Living" src={berlin} alt="" subtitle="Berlin - DE" />
-      <Info title="From" src={brazil} alt="" subtitle="Salvador - BR" />
-      <Info title="Background" src={arch} alt="" subtitle="Architecture" />
-      <Info title="Hobbies" src={art} alt="" subtitle="Arts & Crafts" />
-      <Info title="Languages" src={languages} alt="" subtitle="PT (Native) - EN (B2)" />
+      {ABOUT_INFOS.map(aboutInfo => (
+        <Info
+          key={aboutInfo.src}
+          title={aboutInfo.title}
+          src={aboutInfo.src}
+          alt={aboutInfo.alt}
+          subtitle={aboutInfo.subtitle}
+        />
+      ))}
     </AboutSection>
   </Section>
 );
