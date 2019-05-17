0d9a24c738938bbc148411cc6040ee2e50ebc3e8
MFAC <ninafroes@gmail.com>
2019-05-17 23:03:08 +0200

    Updates

---
+++ precache-manifest.c7ee3cf865bf68a9edf8a7e7ef4d3428.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012893c72e46b55d2937ea0bdae7e224",
+    "url": "/MarinaFroes/Portfolio/index.html"
+  },
+  {
+    "revision": "b7bbc6a8570be0befe85",
+    "url": "/MarinaFroes/Portfolio/static/css/main.f34c7617.chunk.css"
+  },
+  {
+    "revision": "2ae22d52992b6f78ab3e",
+    "url": "/MarinaFroes/Portfolio/static/js/2.bd84e937.chunk.js"
+  },
+  {
+    "revision": "b7bbc6a8570be0befe85",
+    "url": "/MarinaFroes/Portfolio/static/js/main.5efe1eed.chunk.js"
+  },
+  {
+    "revision": "3ca027766ebfd086b93d",
+    "url": "/MarinaFroes/Portfolio/static/js/runtime~main.ece8917d.js"
+  },
+  {
+    "revision": "aec63b7b88bb60db37e0640882ff9482",
+    "url": "/MarinaFroes/Portfolio/static/media/FroggerClone.aec63b7b.png"
+  },
+  {
+    "revision": "72552d06fc5d49df98630e5bec91e1db",
+    "url": "/MarinaFroes/Portfolio/static/media/MarinaCosta.72552d06.png"
+  },
+  {
+    "revision": "491c07c518bf36187204c87fa1143d99",
+    "url": "/MarinaFroes/Portfolio/static/media/MatchingGame.491c07c5.png"
+  },
+  {
+    "revision": "6dbf2d3de29f841978effa039d70ac72",
+    "url": "/MarinaFroes/Portfolio/static/media/PortfolioPlainJS.6dbf2d3d.png"
+  },
+  {
+    "revision": "6108e02dff59be746ef28ef2be6326a7",
+    "url": "/MarinaFroes/Portfolio/static/media/WeatherChecker.6108e02d.png"
+  },
+  {
+    "revision": "9dd105b23777356982034d44dba22f33",
+    "url": "/MarinaFroes/Portfolio/static/media/arch.9dd105b2.svg"
+  },
+  {
+    "revision": "aabdc549c5b0d4534c98b65497f4051c",
+    "url": "/MarinaFroes/Portfolio/static/media/art.aabdc549.svg"
+  },
+  {
+    "revision": "58b3e57b41dfe9e570bb2c77bbd0104a",
+    "url": "/MarinaFroes/Portfolio/static/media/berlin.58b3e57b.svg"
+  },
+  {
+    "revision": "d927fc3dba17646587cd9414d99dd8c4",
+    "url": "/MarinaFroes/Portfolio/static/media/brazil.d927fc3d.svg"
+  },
+  {
+    "revision": "a49b7d606588d47cf494782a3a343ced",
+    "url": "/MarinaFroes/Portfolio/static/media/languages.a49b7d60.svg"
+  }
+]);
