0196478e6da4595d7bf308da42b072b5d00af29f
MFAC <ninafroes@gmail.com>
2019-05-25 08:56:21 +0200

    feat: Add prop types to info

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const InfoContainer = styled.div`
   display: flex;
@@ -18,7 +19,6 @@ const InfoTitle = styled.h3`
 const Image = styled.img`
   height: 100px;
   width: 100px;
-  border-radius: 5px;
   padding: 10px;
 `;
 
@@ -26,8 +26,8 @@ const InfoSubtitle = styled.p`
   font-size: 20px;
 `;
 
-const Info = ({ title, src, alt, subtitle}) => (
-  <InfoContainer >
+const Info = ({ title, src, alt, subtitle }) => (
+  <InfoContainer>
     <InfoTitle>{title}</InfoTitle>
     <Image src={src} alt={alt} />
     <InfoSubtitle>{subtitle}</InfoSubtitle>
@@ -35,3 +35,10 @@ const Info = ({ title, src, alt, subtitle}) => (
 );
 
 export default Info;
+
+Info.propTypes = {
+  title: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string,
+  subtitle: PropTypes.string.isRequired
+};
