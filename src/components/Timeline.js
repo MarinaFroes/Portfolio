6f477f81da6c51fff1d6230bf08509870ecae443
MFAC <ninafroes@gmail.com>
2019-08-26 17:27:52 +0200

    style: Change single to double quotes and add animation

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import Section from './Section';
-import { Timeline, Event } from 'react-timeline-scribble';
-import { EVENTS_INFO } from './eventsInfo';
+import React from "react";
+import Section from "./Section";
+import { Timeline, Event } from "react-timeline-scribble";
+import { EVENTS_INFO } from "./eventsInfo";
+import "animate.css/animate.min.css";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const TimelineContainer = () => (
   <Section heading="Timeline">
     <Timeline>
       {EVENTS_INFO.map(event => (
-        <Event
-          key={event.name}
-          interval={event.interval}
-          title={event.name}
-          subtitle={event.institution}
-        >
-          {event.location !== '' && event.location}
-        </Event>
+        <ScrollAnimation animateIn="fadeIn" duration={2} animateOnce={true}>
+          <Event
+            key={event.name}
+            interval={event.interval}
+            title={event.name}
+            subtitle={event.institution}
+          >
+            {event.location !== "" && event.location}
+          </Event>
+        </ScrollAnimation>
       ))}
     </Timeline>
   </Section>
