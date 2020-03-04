893a2f4ffc9fc06eea0b6c2950b69a87efd82624
MFAC <ninafroes@gmail.com>
2020-03-04 16:22:52 +0100

    styles: Change styles to show additional info onMouseEnter and hidden it onMouseLeave

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,94 +2,40 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ProjectSection = styled.div`
+  display: flex;
   background-image: url(${props => props.imageSrc || 'https://dummyimage.com/740x420/e6e6e6/000000&text=no+image+'});
   background-position: center;
   background-size: cover;
   min-width: 25rem;
   height: auto;
   margin: 0;
-  border: 1px solid red;
+  border: 1px solid #e6e6e6;
   flex-grow: 1;
 `;
-// const ProjectSection = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   background-image: url(${props => props.imageSrc || 'https://dummyimage.com/740x420/e6e6e6/000000&text=no+image+'});
-//   background-position: center;
-//   background-size: cover;
-//   align-items: center;
-//   margin: 0;
-//   border: 2px solid red;
- 
-//   flex: 1 1;
-// `;
 
 const AdditionalInfo = styled.div`
-  height: 100%;
-  width: 100%;
   display: flex;
+  flex: 1;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
   background-color: yellow;
-  visibility: hidden;
+  opacity: 0.85;
+  visibility: ${props => props.isVisible ? 'visible' : 'hidden'};
   width: 15rem;
   height: 15rem;
   font-size: 1.2rem;
-  
-  &:hover {
-    visibility: visible;
-  }
 `;
-
-const ProjectLink = styled.a`
-  text-decoration: none;
-  color: silver;
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-// const Image = styled.img`
-//   height: 200px;
-//   width: auto;
-//   padding: 10px;
-//   border: 1px solid #e6e6e6;
-//   @media only screen and (max-width: 500px){
-//     height: 150px;
-//   }
-// `;
-// const Image = styled.img`
-//   height: 200px;
-//   width: auto;
-//   padding: 10px;
-//   border: 1px solid #e6e6e6;
-//   border-radius: 0 0 20px 0;
-//   @media only screen and (max-width: 500px){
-//     height: 150px;
-//   }
-// `;
 
 const ProjectTitle = styled.p`
   font-size: 1.5rem;
   text-transform: uppercase;
   font-weight: bold;
   text-align: center;
-  padding: 0.25rem;
-  background-color: silver;
-  color: white;
+  padding: 0.1rem;
+  color: purple;
 `;
-// const ProjectTitle = styled.p`
-//   font-size: 1.5rem;
-//   text-transform: uppercase;
-//   font-weight: bold;
-//   text-align: center;
-//   padding: 0.25rem;
-//   background-color: silver;
-//   color: white;
-//   border-radius: 20px 0 0 0;
-// `;
 
 const LinksContainer = styled.div`
   display: flex;
@@ -98,25 +44,55 @@ const LinksContainer = styled.div`
 `;
 
 const Link = styled.a`
+  font-size: 1rem;
   text-decoration: none;
-  color: black;
+  color: #000;
+  padding: 0.8rem;
+  border: 2px solid purple;
   &:hover {
-    text-decoration: underline;
+    background-color: purple;
+    color: #fff;
   }
 `;
 
-const Project = ({ title, src, alt, project, code }) => (
-  <ProjectSection imageSrc={src}>
-    <ProjectLink href={project} target="_blank" rel="noopener noreferrer">
-    <AdditionalInfo >
-      <LinksContainer>
-        <Link href={project} target="_blank" rel="noopener noreferrer">View Project</Link>
-        <Link href={code} target="_blank" rel="noopener noreferrer">View Code</Link>
-      </LinksContainer>
-      <ProjectTitle>{title}</ProjectTitle>
-    </AdditionalInfo>
-    </ProjectLink>
-  </ProjectSection>
-);
+const Technologies = styled.p`
+  font-size: 1.1rem;
+`
+
+class Project extends React.Component{
+  state = {
+    isVisible: false
+  }
+
+  toggleVisibility(trueOrFalse) {
+    this.setState({
+      isVisible: trueOrFalse
+    })
+  }
+
+  render() {
+    const { src, project, code, title, technologies } = this.props
+    const { isVisible } = this.state
+
+    return (
+      <ProjectSection
+        imageSrc={src}
+        onMouseEnter={() => this.toggleVisibility(true)}
+        onMouseLeave={() => this.toggleVisibility(false)}
+      >
+        <AdditionalInfo
+          isVisible={isVisible}
+        >
+          <ProjectTitle>{title}</ProjectTitle>
+          <Technologies><strong>Technologies:</strong> {technologies}</Technologies>
+          <LinksContainer>
+            <Link href={project} target="_blank" rel="noopener noreferrer">View Project</Link>
+            <Link href={code} target="_blank" rel="noopener noreferrer">View Code</Link>
+          </LinksContainer>
+        </AdditionalInfo>
+      </ProjectSection>
+    )
+  }
+}
 
 export default Project;
