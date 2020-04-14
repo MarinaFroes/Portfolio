79dec96e7e906eb6e100e026d3d4199c4c199c6a
MFAC <ninafroes@gmail.com>
2020-04-14 17:00:48 +0200

    feat: Remove project info and import it from utils file

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -4,76 +4,10 @@ import ScrollAnimation from "react-animate-on-scroll";
 
 import Section from "./Section";
 import Project from "./Project";
-import MatchingGame from "../assets/images/MatchingGame.png";
-import FroggerClone from "../assets/images/FroggerClone.png";
-import Portfolio from "../assets/images/PortfolioPlainJS.png";
-import WeatherChecker from "../assets/images/WeatherChecker.png";
-import YelpClone from "../assets/images/YelpClone.png";
-import TeaCozy from "../assets/images/TeaCozy.png";
-import WouldYouRather from "../assets/images/WouldYouRather.png";
-import MyReads from "../assets/images/MyReads.png";
+import { PROJECTS_INFO } from "../utils/projectInfo";
 
 const MY_PROFILE = "https://github.com/MarinaFroes";
 const MY_GITHUB = "https://marinafroes.github.io";
-
-const PROJECTS_INFO = [
-  {
-    title: "My Reads App",
-    src: `${MyReads}`,
-    alt: "Screenshot of the My Reads Project",
-    link: "/RDND-project1-MyReads/#/",
-    technologies: 'React.js'
-  },
-  {
-    title: "Would You Rather App",
-    src: `${WouldYouRather}`,
-    alt: "Screenshot of the Would You Rather  Project",
-    link: "/RDND-project2-WouldYouRather/#/",
-    technologies: 'React.js, Redux'
-  },
-  {
-    title: "Frogger Clone",
-    src: `${FroggerClone}`,
-    alt: "Screenshot of the Frogger Clone Project",
-    link: "/FEND-project3-Arcade-Game-Clone/",
-    technologies: 'JavaScript, CSS3, HTML5'
-  },
-  {
-    title: "Matching Game",
-    src: `${MatchingGame}`,
-    alt: "Screenshot of the Matching Game Project",
-    link: `/FEND-project2-Matching-Game/`,
-    technologies: 'JavaScript, CSS3, HTML5'
-  },
-  {
-    title: "Portfolio webpage",
-    src: `${Portfolio}`,
-    alt: "Screenshot of the Portfolio webpage",
-    link: `/FEND-project1-Portfolio/`,
-    technologies: 'CSS3, HTML5'
-  },
-  {
-    title: "Yelp Clone",
-    src: `${YelpClone}`,
-    alt: "Screenshot of the Yelp Clone Project",
-    link: `/Ravenous-Codecademy/`,
-    technologies: 'React.js'
-  },
-  {
-    title: "Weather Checker",
-    src: `${WeatherChecker}`,
-    alt: "Screenshot of the Weather Checker Project",
-    link: `/Check-Weather/`,
-    technologies: 'React.js'
-  },
-  {
-    title: "Tea Cozy",
-    src: `${TeaCozy}`,
-    alt: "Screenshot of the Tea Cozy Project",
-    link: `/Tea-Cozy/`,
-    technologies: 'React.js'
-  }
-];
 
 const ProjectsSection = styled.div`
   display: flex;
@@ -86,7 +20,7 @@ const Projects = () => (
   <Section heading="Projects">
     <ProjectsSection>
       {PROJECTS_INFO.map((project, index) => (
-        <ScrollAnimation key={index} animateIn="fadeIn" duration={2} animateOnce={true}>
+        <ScrollAnimation key={index} animateIn="fadeIn" duration={2} animateOnce={false}>
           <Project
             title={project.title}
             src={project.src}
