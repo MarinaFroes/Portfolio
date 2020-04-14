fb4bfd048bc0b42ccedc824ee7b5dc643a1b274a
MFAC <ninafroes@gmail.com>
2020-04-14 16:20:25 +0200

    feat: Add photo

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,7 @@ import ScrollAnimation from "react-animate-on-scroll";
 import Section from "./Section";
 import Info from "./Info";
 import { ABOUT_INFOS } from "../utils/aboutInfo";
+import picture from '../assets/images/MarinaCosta.jpg'; 
 
 const AboutSection = styled.div`
   display: flex;
@@ -14,8 +15,30 @@ const AboutSection = styled.div`
   width: 100%;
 `;
 
+const Image = styled.img`
+  width: 6rem;
+  height: 6rem;
+  border-radius: 50%;
+  box-shadow: 4px 3px 14px 0 #455369;
+
+  @media only screen and (max-width: 900px){
+    width: 30vw;
+  }
+
+  @media only screen and (max-width: 700px){
+    width: 30vw;
+  }
+
+  @media only screen and (max-width: 600px){
+    width: 50vw;
+  }
+
+`;
+
+
 const About = () => (
   <Section heading="About Me">
+    <Image src={picture} alt="Profile picture" />
     <AboutSection>
       {ABOUT_INFOS.map((aboutInfo, index) => (
         <ScrollAnimation key={index} animateIn="fadeIn" duration={2} animateOnce={true}>
