58426877da8fc4a9d2421da214dc0eb0ed592c5d
MFAC <ninafroes@gmail.com>
2020-04-14 18:47:49 +0200

    feat: Add info and skills

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,72 @@
 import React from "react";
 import styled from "styled-components";
-import "animate.css/animate.min.css";
-import ScrollAnimation from "react-animate-on-scroll";
 
 import Section from "./Section";
-import Info from "./Info";
-import { ABOUT_INFOS } from "../utils/aboutInfo";
-import picture from '../assets/images/MarinaCosta.jpg'; 
+import picture from "../assets/images/MarinaCosta.jpg"; 
+import SkillsContainer from "./SkillsContainer";
 
-const AboutSection = styled.div`
+
+const AboutMeContainer = styled.div`
+  box-shadow: 4px 3px 14px 0 rgba(179, 179, 204, 1);
+  border-radius: 0.5rem;
   display: flex;
-  justify-content: space-evenly;
-  flex-flow: row wrap;
+  width: 90%;
+  margin-top: 2rem;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+
+  @media only screen and (max-width: 700px){
+    width: 80%;
+  }
+`;
+
+const UserDiv = styled.div`
+  padding: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-right: 2px solid #e6e6e6;
+`
+
+const AboutMeText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+`;
+
+const Heading = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding: 0 2rem;
+  margin: 0;
+  margin-top: 2rem;
+`;
+
+const Skills = styled.div`
   width: 100%;
 `;
 
+const Info = styled.div`
+  text-align: left;
+`;
+
+const Paragraph = styled.p`
+  padding: 0.5rem 2rem;
+  margin: 0;
+  font-size: 1rem;
+`;
+
 const Image = styled.img`
-  width: 6rem;
-  height: 6rem;
+  width: 30%;
+  flex-grow: 1;
+  margin: 1rem;
   border-radius: 50%;
   box-shadow: 4px 3px 14px 0 #455369;
-
-  @media only screen and (max-width: 900px){
+`;
+/**
+ *   @media only screen and (max-width: 900px){
     width: 30vw;
   }
 
@@ -32,14 +77,36 @@ const Image = styled.img`
   @media only screen and (max-width: 600px){
     width: 50vw;
   }
-
-`;
-
+ */
 
 const About = () => (
-  <Section heading="About Me">
-    <Image src={picture} alt="Profile picture" />
-    <AboutSection>
+  <Section heading="About">
+    <AboutMeContainer>
+      <UserDiv>
+        <Image src={picture} alt="Profile picture" />
+      </UserDiv>
+      <AboutMeText>
+        <Info>
+          <Heading>A brief introduction</Heading>
+          <Paragraph>
+            I am a <strong>Junior Front-End Developer</strong> with background is in Civil Architecture currently living in <strong>Berlin, Germany</strong>.
+          </Paragraph>
+          <Paragraph>
+            I aspire to work in a great company where I can leverage my knowledge of Web Development Technologies and contribute to its operations. Therefore, I welcome new opportunities for Front-End or Web Development roles.
+          </Paragraph>
+          <Paragraph>
+            You can check a few of my projects bellow.
+          </Paragraph>
+        </Info>
+        <Skills>
+          <Heading>Main skills</Heading>
+          <SkillsContainer/>
+        </Skills>
+      </AboutMeText>
+    </AboutMeContainer>
+    
+    
+    {/* <AboutSection>
       {ABOUT_INFOS.map((aboutInfo, index) => (
         <ScrollAnimation key={index} animateIn="fadeIn" duration={2} animateOnce={true}>
           <Info
@@ -51,7 +118,7 @@ const About = () => (
             />
         </ScrollAnimation>
       ))}
-    </AboutSection>
+    </AboutSection> */}
   </Section>
 );
 
