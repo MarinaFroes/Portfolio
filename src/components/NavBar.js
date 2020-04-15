02890cd599ff0e31e4d5b059ba837cc4bb06fa82
MFAC <ninafroes@gmail.com>
2020-04-15 19:50:50 +0200

    styles: Adjust media queries for the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,23 +4,6 @@ import styled from 'styled-components';
 import { LINKS } from '../utils/links';
 import logo from '../assets/icons/logo.svg';
 
-
-const NavLink = styled.a`
-  color: #000;
-  text-decoration: none;
-  margin: 1rem;
-  font-size: 1.1rem;
-  border: 2px solid red;
-
-  &:hover {
-    text-decoration: underline;
-  }
-
-  @media only screen and (max-width: 500px){
-    margin: 0.5rem;
-  }
-`;
-
 const StyledNav = styled.nav`
   display: flex;
   justify-content: space-between;
@@ -29,15 +12,7 @@ const StyledNav = styled.nav`
   margin: 0;
   width: 100%;
   height: 5rem;
-  border: 2px solid blue;
 `
-
-// display: flex;
-// flex - direction: column;
-// margin: 0;
-// padding: 2rem 0;
-// width: 100 %;
-// border: 2px solid red;
 
 const StyledLogo = styled.a`
   display: flex;
@@ -46,14 +21,13 @@ const StyledLogo = styled.a`
   text-decoration: none;
   margin: 0;
   margin-left: 2rem;
-  border: 2px solid green;
 
-  @media only screen and (max-width: 500px){
+  @media only screen and (max-width: 270px){
     display: none;
   }
 `;
 
-const Icon = styled.img`
+const Logo = styled.img`
   height: 3rem;
   margin: 0;
 `;
@@ -63,16 +37,27 @@ const LinksContainer = styled.div`
   flex-wrap: wrap;
   justify-content: space-evenly;
   align-items: center;
-  border: 2px solid purple;
+`;
+
+const NavLink = styled.a`
+  color: #000;
+  text-decoration: none;
+  margin: 1rem;
+  font-size: 1.5rem;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  @media only screen and (max-width: 300px){
+    margin: 0.5rem;
+  }
 `;
 
 const NavBar = () => (
   <StyledNav>
-    <StyledLogo
-      href="/"
-      rel="noopener noreferrer"
-    >
-      <Icon src={logo} alt="" />
+    <StyledLogo href="/" rel="noopener noreferrer">
+      <Logo src={logo} alt="" />
     </StyledLogo>
     <LinksContainer>
       {LINKS.map(link => (
