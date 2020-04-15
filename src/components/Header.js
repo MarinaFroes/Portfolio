d759c12dd0cb994c6018d614beebde42f05219ff
MFAC <ninafroes@gmail.com>
2020-04-15 19:16:50 +0200

    feat: Add responsive image and change text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// import picture from '../assets/images/MarinaCosta.jpg'; 
-import { LINKS } from '../utils/links';
-import logo from '../assets/icons/logo.svg';
-import BgImg from '../assets/images/laptop-1483974_1920.jpg';
+import NavBar from './NavBar';
+import BgImg1920 from '../assets/images/laptop_1920.jpg';
+import BgImg1600 from '../assets/images/laptop_1600.jpg';
+import BgImg1440 from '../assets/images/laptop_1440.jpg';
+import BgImg1366 from '../assets/images/laptop_1366.jpg';
+import BgImg1024 from '../assets/images/laptop_1024.jpg';
+import BgImg768 from '../assets/images/laptop_768.jpg';
+import BgImg640 from '../assets/images/laptop_640.jpg';
 
 const Header = styled.header`
   display: flex;
@@ -13,139 +17,113 @@ const Header = styled.header`
   width: 100%;
 `;
 
-const NavContainer = styled.div`
+const HeroImage = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 2rem;
-  height: 10vh;
-`;
-
-const NavLink = styled.a`
-  color: #000;
-  text-decoration: none;
-  margin: 1rem;
-  font-size: 1.1rem;
-  &:hover {
-    text-decoration: underline;
-  }
-
-  @media only screen and (max-width: 500px){
-    margin: 0.5rem;
-  }
-`;
-
-const StyledNav = styled.nav`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-`
-
-const AuthorInfo = styled.div`
-  display: flex;
-  background-color: eee;
-  background-image: url(${BgImg});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
   text-align: center;
-  height: 90vh;
+  height: 90%;
+  border: 2px solid red;
 `;
 
-// const Image = styled.img`
-//   width: 20vw;
-//   height: auto;
-//   border-radius: 50%;
-//   box-shadow: 4px 3px 14px 0 #455369;
-
-//   @media only screen and (max-width: 900px){
-//     width: 30vw;
-//   }
-
-//   @media only screen and (max-width: 700px){
-//     width: 30vw;
-//   }
-
-//   @media only screen and (max-width: 600px){
-//     width: 50vw;
-//   }
-
-// `;
-
-const AuthorText = styled.div`
+const HeadingContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 2rem 0;
-`
-  // background: rgba(255, 255, 255, 0.8);
+  position: absolute;
+  z-index: 100;
+  background: rgba(255, 255, 255, 0.8);
 
-const Title = styled.h2`
+  @media only screen and (max-width: 700px){
+    padding: 1.2rem 0;
+  }
+  @media only screen and (max-width: 500px){
+    padding: 1rem 0;
+  }
+`
+
+const Heading = styled.h2`
   color: #000;
   font-size: 1.6rem;
   font-weight: normal;
   padding: 0 2rem 2rem 2rem;
   margin: 0;
+  text-shadow: 2px 2px #fff;
+
+  @media only screen and (max-width: 700px){
+    font-size: 1.2rem;
+  }
+  @media only screen and (max-width: 500px){
+    font-size: 1rem;
+  }
 `;
 
-const Greeting = styled.h1`
-  color: #000;
-  font-size: 2rem;
-  font-weight: normal;
-  padding: 2rem 2rem 0 2rem;
-  margin: 0;
-`
-
-const StyledLogo = styled.a`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-decoration: none;
-  margin: 0;
-
-  @media only screen and (max-width: 500px){
-    display: none;
-  }
-`
-
-const Icon = styled.img`
-  height: 5vh;
-  margin: 0;
-`
 
 const HeaderContainer = () => (
   <Header>
-    <NavContainer >
-      <StyledLogo
-        href="/"
-        rel="noopener noreferrer"
-      >
-        <Icon src={logo} alt="" />
-      </StyledLogo>
-      <StyledNav>
-        {LINKS.map(link => (
-          <NavLink
-            key={link.href}
-            href={link.href}
-          >
-            {link.label}
-          </NavLink>
-        ))}
-      </StyledNav>
-      
-    </NavContainer>
-    <AuthorInfo>
-      <AuthorText>
-        <Greeting>Hello, I'm <strong>Marina Costa</strong>.</Greeting>
-        <Title>I am a Frontend Developer</Title>
-      </AuthorText>
-      {/* <Image src={picture} alt="Profile picture" /> */}
-    </AuthorInfo>
+    <NavBar />
+    <HeroImage>
+      <img
+        src={BgImg640}
+        srcSet={`${BgImg1920} 1920w, ${BgImg1600} 1600w, ${BgImg1440} 1440w, ${BgImg1366} 1366w, ${BgImg1024} 1024w, ${BgImg768} 768w, ${BgImg640} 640w`}
+        size="100vw"
+        alt=""
+      />
+      <HeadingContainer>
+        <Heading>Frontend Development Portfolio</Heading>
+      </HeadingContainer>
+    </HeroImage>
   </Header>
 );
 
 export default HeaderContainer;
+
+/**
+ * <FullsizePicture
+        src={BgImg1024}
+      >
+        <AuthorText>
+          <Greeting>Hello, I'm <strong>Marina Costa</strong>.</Greeting>
+          <Title>I am a Frontend Developer</Title>
+        </AuthorText>
+      </FullsizePicture>
+
+
+      <img
+        src={BgImg640}
+        srcSet={`${BgImg1920} 1920w, ${BgImg1600} 1600w, ${BgImg1440} 1440,w ${BgImg1366} 1366w, ${BgImg1024} 1024w, ${BgImg768} 768w, ${BgImg640} 640w`}
+        alt=""
+      />
+ */
+/**
+ * sources={[
+          {
+            srcSet: `${BgImg1920} 1920w, ${BgImg1600} 1600w, ${BgImg1440} 1440,w ${BgImg1366} 1366w, ${BgImg1024} 1024w, ${BgImg768} 768w, ${BgImg640} 640w`
+          }
+        ]}
+ */
+
+ /**
+  * srcSet={`${BgImg1920} 1920w, ${BgImg1600} 1600w, ${BgImg1440} 1440,w ${BgImg1366} 1366w, ${BgImg1024} 1024w, ${BgImg768} 768w, ${BgImg640} 640w`}
+  */
+
+  /**
+   * <img
+        src={BgImg640}
+        srcSet = {`${BgImg1920} 1920w, ${BgImg1600} 1600w, ${BgImg1440} 1440,w ${BgImg1366} 1366w, ${BgImg1024} 1024w, ${BgImg768} 768w, ${BgImg640} 640w`}
+        sizes="100vw"
+        alt=""
+      />
+
+      <AuthorText>
+          <Greeting>Hello, I'm <strong>Marina Costa</strong>.</Greeting>
+          <Title>I am a Frontend Developer</Title>
+        </AuthorText>
+
+        srcSet={`${BgImg1920} 1920w, ${BgImg1600} 1600w, ${BgImg1440} 1440,w ${BgImg1366} 1366w, ${BgImg1024} 1024w, ${BgImg768} 768w, ${BgImg640} 640w`}
+        sizes="100vw"
+        cover="height"
+        center={true}
+   */
