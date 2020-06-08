d4d3c2f528d13d86a3ebf172e1dda0cff6439f08
MFAC <ninafroes@gmail.com>
2020-06-08 12:01:00 +0200

    feat: Add dev.to badge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { FaGithub, FaLinkedin, FaEnvelope, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope, FaTwitter, FaDev } from "react-icons/fa";
 
 import Section from "./Section";
 import SocialMedia from "./SocialMedia";
@@ -9,6 +9,7 @@ const MY_LINKEDIN = "https://www.linkedin.com/in/marina-froes-a-costa/";
 const MY_GITHUB = "https://github.com/MarinaFroes";
 const MY_GMAIL = "facosta.marina@gmail.com";
 const MY_TWITTER = "https://twitter.com/facosta_marina";
+const MY_DEV = "https://dev.to/marinafroes";
 
 const Contacts = styled.footer`
   display: flex;
@@ -47,6 +48,8 @@ const Footer = () => (
         <SocialMedia icon={<FaGithub />} address={MY_GITHUB} />
         <SocialMedia icon={<FaLinkedin />} address={MY_LINKEDIN} />
         <SocialMedia icon={<FaTwitter />} address={MY_TWITTER} />
+        <SocialMedia icon={<FaDev />} address={MY_DEV} />
+
       </SocialMediaContainer>
     </Contacts>
   </Section>
