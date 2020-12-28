a77d284b1686f818dd60e0290e635d5e4a046e33
MFAC <ninafroes@gmail.com>
2020-12-28 14:12:15 +0100

    [feat] Update about me

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,7 +3,16 @@ import React from 'react'
 import Section from '../Section'
 import MyPic from '../../assets/images/MarinaCosta.jpg'
 import SkillsContainer from '../SkillsContainer'
-import { AboutMeContainer, UserDiv, AboutMeText, Heading, Skills, Info, Paragraph, Image } from './styles'
+import {
+  AboutMeContainer,
+  UserDiv,
+  AboutMeText,
+  Heading,
+  Skills,
+  Info,
+  Paragraph,
+  Image,
+} from './styles'
 
 const About = () => (
   <Section heading='About me'>
@@ -15,14 +24,9 @@ const About = () => (
         <Info>
           <Heading>Hello, I'm Marina!</Heading>
           <Paragraph>
-            I am a <strong>Fullstack Developer @ Integrify</strong> with
-            background in Architecture currently living in
-            <strong> Berlin, Germany</strong>.
-          </Paragraph>
-          <Paragraph>
-            Do you want to work with me? Integrify provides consulting and
-            recruitment services for companies. Let’s talk more via Joona
-            Ohra-aho: <a href="mailto:joona@integrify.io"><strong>joona@integrify.io</strong></a> / +358 400 579 588.
+            I am a <strong>Fullstack Developer</strong> with a focus on{' '}
+            <strong>Frontend</strong> and background in Architecture currently
+            living in <strong> Berlin, Germany</strong>.
           </Paragraph>
           <Paragraph>You can check a few of my projects bellow.</Paragraph>
         </Info>
